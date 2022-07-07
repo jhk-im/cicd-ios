2d62e6f11acece861991b91045f65fda6f2c4c93
jhk-im <kjhun200@gmail.com>
2022-07-07 15:26:45 +0900

    first commit

---
+++ Shared/cicd_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicd_iosApp.swift
+//  Shared
+//
+//  Created by HUN on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct cicd_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
